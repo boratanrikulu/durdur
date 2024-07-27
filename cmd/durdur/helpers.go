5a9273121d10f5bbfbde83734bbe9a0bac453c93
boratanrikulu <me@bora.sh>
2024-07-27 19:28:37 +0300

    do not use google.com in tests

--- cmd/durdur/helpers.go
+++ cmd/durdur/helpers.go
@@ -8,6 +8,10 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+func writeLn(c *cli.Context, format string, a ...any) error {
+	return write(c, format+"\n", a...)
+}
+
 func write(c *cli.Context, format string, a ...any) error {
 	_, err := io.WriteString(c.App.Writer, fmt.Sprintf(format, a...))
 	return err
@@ -19,5 +23,5 @@ func marshalAndWrite(c *cli.Context, list any) error {
 		return fmt.Errorf("marshal src list: %w", err)
 	}
 
-	return write(c, string(b))
+	return writeLn(c, string(b))
 }
